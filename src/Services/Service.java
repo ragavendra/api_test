519eaa8efc821e9e47fc05da4631c814e9f503b5
ragavendra <ragavendra.bn@gmail.com>
2017-09-18 13:29:01 -0700

    segregating service calls only code

--- src/Services/Service.java
+++ src/Services/Service.java
@@ -1,4 +1,4 @@
-class Service {
+class Service implements PostCalls {
 
 	public static args;
 
@@ -16,36 +16,11 @@ class Service {
 		//requestURL
 		String requestUrl="https://git.eclipse.org/r/gerrit/rpc/ChangeDetailService";
 		
+		//declare or initialize instance variable
+		PostCalls response = new PostCalls();
+		
 		//Perform POST call
 		response = sendPostRequest(requestUrl, payload);
-	}
-
-	//POST action
-	public static String sendPostRequest(String requestUrl, String payload) {
-		try {
-			URL url = new URL(requestUrl);
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-			connection.setDoInput(true);
-			connection.setDoOutput(true);
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("Accept", "application/json");
-			connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-			OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
-			writer.write(payload);
-			writer.close();
-			BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer jsonString = new StringBuffer();
-			String line;
-			while ((line = br.readLine()) != null) {
-				jsonString.append(line);
-			}
-			br.close();
-			connection.disconnect();
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage());
-		}
-		return jsonString.toString();
 	}
 
 }
